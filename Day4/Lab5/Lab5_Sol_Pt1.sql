43a407a3418b2bd77f2ac17612b768498f32defc
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2025-08-19 15:38:15 +0300

    Update Lab5_Sol_Pt1.sql

--- Day4/Lab5/Lab5_Sol_Pt1.sql
+++ Day4/Lab5/Lab5_Sol_Pt1.sql
@@ -1,5 +1,90 @@
 -- lab 5 part 1 ITI database
 
+SELECT COUNT(*) AS StudentsWithAge
+FROM Student
+WHERE st_Age IS NOT NULL;
+
+
+SELECT DISTINCT ins_Name
+FROM Instructor;
+
+
+SELECT 
+  S.st_ID AS StudentID,
+  ISNULL(S.st_Fname, '') + ' ' + ISNULL(S.st_Lname, '') AS FullName,
+  ISNULL(D.dept_Name, 'No Department') AS DepartmentName
+FROM Student S
+LEFT JOIN Department D ON S.Dept_Id = D.dept_ID;
+
+
+SELECT 
+  S.st_Fname + ' ' + S.st_Lname AS StudentFullName,
+  C.crs_Name AS CourseName
+FROM Student S
+JOIN Stud_Course SC ON S.st_ID = SC.St_Id
+JOIN Course C ON SC.Crs_ID = C.crs_ID
+WHERE SC.Grade IS NOT NULL;
+
+
+SELECT 
+  T.Name AS TopicName,
+  COUNT(C.ID) AS CourseCount
+FROM Topic T
+LEFT JOIN Course C ON T.ID = C.Top_ID
+GROUP BY T.Name;
+
+
+
+
+SELECT 
+  MAX(Salary) AS MaxSalary,
+  MIN(Salary) AS MinSalary
+FROM Instructor;
+
+
+--aggregation no need for is null
+SELECT *
+FROM Instructor
+WHERE Salary < (SELECT AVG(Salary) FROM Instructor );
+
+
+
+SELECT TOP 1 D.dept_Name AS DepartmentName
+FROM Instructor I
+JOIN Department D ON I.Dept_ID = D.dept_ID
+WHERE I.Salary = (SELECT MIN(Salary) FROM Instructor WHERE Salary IS NOT NULL);
+
+
+
+
+SELECT DISTINCT TOP 2 Salary
+FROM Instructor
+WHERE Salary IS NOT NULL
+ORDER BY Salary DESC;
+
+
+
+
+SELECT 
+  ins_Name,
+  COALESCE(CAST(Salary AS VARCHAR), CAST(Bonus AS VARCHAR)) AS Compensation
+FROM Instructor;
+
+
+
+SELECT AVG(Salary) AS AverageSalary
+FROM Instructor
+WHERE Salary IS NOT NULL;
+
+
+
+SELECT 
+  S.st_Fname AS StudentFirstName, I.*
+FROM Student S
+LEFT JOIN Instructor I ON S.st_Super = I.ins_ID;
+
+
+
 CREATE VIEW V_StudentPassedCourses AS
 SELECT 
   S.St_Fname + ' ' + S.St_Lname AS StudentFullName,
@@ -29,3 +114,4 @@ SELECT
 FROM Instructor I
 JOIN Department D ON I.Dept_Id = D.Dept_Id
 WHERE D.Dept_Name IN ('SD', 'Java');
+
